b5c93f69120f1206cf38b89e1bf1ef3aebf8a201
Ian-Garrigan <igarrigan@hotmail.com>
2022-10-19 22:50:12 +0000

    Declare variable & game object array

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+// Declaring const variables for game
+const section = document.querySelector('section');
+const playerLives = document.querySelector('playerLives');
+const playerLivesCat = 9;
+
+// Setting the playersLives text content 
+playerLives.textContent = playerLivesCat;
+
+// Created an array of objects for my cards
+const getData = () => [{
+        imgSrc: '/assets/images/scary-pumpkin.png',
+        name: 'scary pumpkin'
+    },
+    {
+        imgSrc: '/assets/images/dracula.png',
+        name: 'dracula'
+    },
+    {
+        imgSrc: '/assets/images/ghost.png',
+        name: 'ghost'
+    },
+    {
+        imgSrc: '/assets/images/grim-reaper.png',
+        name: 'grim reaper'
+    },
+    {
+        imgSrc: '/assets/images/frankenstein.png',
+        name: 'frankenstein'
+    },
+    {
+        imgSrc: '/assets/images/mummy.png',
+        name: 'mummy'
+    },
+    {
+        imgSrc: '/assets/images/wicked-witch.png',
+        name: 'wicked witch'
+    },
+    {
+        imgSrc: '/assets/images/skeleton.png',
+        name: 'skeleton'
+    },
+    {
+        imgSrc: '/assets/images/scary-clown.png',
+        name: 'scary clown'
+    }
+];
