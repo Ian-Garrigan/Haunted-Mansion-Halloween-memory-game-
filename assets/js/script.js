9122288b13948e912e91bffba534b1b41e828752
Ian-Garrigan <igarrigan@hotmail.com>
2022-10-20 17:09:35 +0000

    Adjust card array

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,13 +42,46 @@ const getData = () => [{
     {
         imgSrc: '/assets/images/scary-clown.png',
         name: 'scary clown'
+    },
+    {
+        imgSrc: '/assets/images/scary-pumpkin.png',
+        name: 'scary pumpkin'
+    },
+    {
+        imgSrc: '/assets/images/dracula.png',
+        name: 'dracula'
+    },
+    {
+        imgSrc: '/assets/images/ghost.png',
+        name: 'ghost'
+    },
+    {
+        imgSrc: '/assets/images/grim-reaper.png',
+        name: 'grim reaper'
+    },
+    {
+        imgSrc: '/assets/images/frankenstein.png',
+        name: 'frankenstein'
+    },
+    {
+        imgSrc: '/assets/images/mummy.png',
+        name: 'mummy'
+    },
+    {
+        imgSrc: '/assets/images/wicked-witch.png',
+        name: 'wicked witch'
+    },
+    {
+        imgSrc: '/assets/images/skeleton.png',
+        name: 'skeleton'
+    },
+    {
+        imgSrc: '/assets/images/scary-clown.png',
+        name: 'scary clown'
     }
 ];
 
 // Function for retrieving the cards in a random order
 const randomize = () => {
 const cardData = getData();
-console.log(cardData);
 }
-
-randomize();
