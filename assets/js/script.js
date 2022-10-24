75000e6845e6adb6d678d0d18b8d3a0834fd3453
Ian-Garrigan <igarrigan@hotmail.com>
2022-10-24 14:33:51 +0000

    Fix relative path issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,75 +8,75 @@ playerLives.textContent = playerLivesCat;
 
 // Created an array of objects for my cards
 const getData = () => [{
-        imgSrc: '/assets/images/scary-pumpkin.png',
+        imgSrc: 'assets/images/scary-pumpkin.png',
         name: 'scary pumpkin'
     },
     {
-        imgSrc: '/assets/images/dracula.png',
+        imgSrc: 'assets/images/dracula.png',
         name: 'dracula'
     },
     {
-        imgSrc: '/assets/images/ghost.png',
+        imgSrc: 'assets/images/ghost.png',
         name: 'ghost'
     },
     {
-        imgSrc: '/assets/images/grim-reaper.png',
+        imgSrc: 'assets/images/grim-reaper.png',
         name: 'grim reaper'
     },
     {
-        imgSrc: '/assets/images/frankenstein.png',
+        imgSrc: 'assets/images/frankenstein.png',
         name: 'frankenstein'
     },
     {
-        imgSrc: '/assets/images/mummy.png',
+        imgSrc: 'assets/images/mummy.png',
         name: 'mummy'
     },
     {
-        imgSrc: '/assets/images/wicked-witch.png',
+        imgSrc: 'assets/images/wicked-witch.png',
         name: 'wicked witch'
     },
     {
-        imgSrc: '/assets/images/skeleton.png',
+        imgSrc: 'assets/images/skeleton.png',
         name: 'skeleton'
     },
     {
-        imgSrc: '/assets/images/scary-clown.png',
+        imgSrc: 'assets/images/scary-clown.png',
         name: 'scary clown'
     },
     {
-        imgSrc: '/assets/images/scary-pumpkin.png',
+        imgSrc: 'assets/images/scary-pumpkin.png',
         name: 'scary pumpkin'
     },
     {
-        imgSrc: '/assets/images/dracula.png',
+        imgSrc: 'assets/images/dracula.png',
         name: 'dracula'
     },
     {
-        imgSrc: '/assets/images/ghost.png',
+        imgSrc: 'assets/images/ghost.png',
         name: 'ghost'
     },
     {
-        imgSrc: '/assets/images/grim-reaper.png',
+        imgSrc: 'assets/images/grim-reaper.png',
         name: 'grim reaper'
     },
     {
-        imgSrc: '/assets/images/frankenstein.png',
+        imgSrc: 'assets/images/frankenstein.png',
         name: 'frankenstein'
     },
     {
-        imgSrc: '/assets/images/mummy.png',
+        imgSrc: 'assets/images/mummy.png',
         name: 'mummy'
     },
     {
-        imgSrc: '/assets/images/wicked-witch.png',
+        imgSrc: 'assets/images/wicked-witch.png',
         name: 'wicked witch'
     },
     {
-        imgSrc: '/assets/images/skeleton.png',
+        imgSrc: 'assets/images/skeleton.png',
         name: 'skeleton'
     },
     {
-        imgSrc: '/assets/images/scary-clown.png',
+        imgSrc: 'assets/images/scary-clown.png',
         name: 'scary clown'
     }
 ];
